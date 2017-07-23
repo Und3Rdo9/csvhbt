9928d55d572dfecefb8f8ef9e491d6f809a4a564
Tim Bakkum <timbakkum@gmail.com>
2017-07-23 17:09:36 +0200

    initial commit

---
+++ wp-content/themes/csv-handbal/Gruntfile.js
@@ -0,0 +1,153 @@
+/*global module:false*/
+/* jshint node: true */
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // Task configuration.
+
+    sass: {                              
+      dist: {                            
+        options: {                       
+          style: 'expanded',
+          trace: true,
+
+        },
+        files: {                         
+          'src/stylesheets/build/main.compiled.css': 'src/stylesheets/scss/main.scss', // 'destination': 'source'
+          
+        }
+      }
+    },
+    postcss: {
+      options: {
+        map: true, 
+        processors: [
+          require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
+          require('cssnano')() // minify the result
+        ]
+      },
+      dist: {
+        src: 'src/stylesheets/build/main.compiled.css',
+        dest: 'src/stylesheets/build/main.min.css'
+      }
+    },
+    uglify: {
+      options: {
+        sourceMap: true,
+        compress: {},
+      },
+      dist: {
+        src: ['src/bower_components/Headhesive.js/dist/headhesive.js', 'src/bower_components/flickity/dist/flickity.pkgd.min.js', 'src/bower_components/smooth-scroll/dist/js/smooth-scroll.js', 'src/scripts/main.js'],
+        dest: 'src/scripts/build/main.min.js'
+      }
+    },
+    jshint: {
+
+      options: {
+        curly: true,
+        eqeqeq: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        devel: true,
+        reporterOutput: '', 
+        globals: {
+          jQuery: true
+        }
+
+      },
+      main: {
+        src: ['src/scripts/*.js']
+      },
+      gruntfile: {
+        src: ['Gruntfile.js']
+      },
+
+    },
+    modernizr:{
+      dist: {
+        "parseFiles": true,
+        "crawl" : true,
+        "customTests": [],
+        "devFile": false,
+        "dest": "src/scripts/build/modernizr-output.js",
+        "tests": [
+          // Tests
+        ],
+        "options": [
+          "setClasses"
+        ],
+        "uglify": true
+      },
+    },
+    htmlhint: {
+      html1: {
+        options: {
+          'tag-pair': true
+        },
+        src: ['src/*.html']
+      },
+    },
+    watch: {
+      gruntfile: {
+        files: 'Gruntfile.js',
+        tasks: ['jshint:gruntfile']
+      },
+      main: {
+        files: 'src/scripts/main.js',
+        tasks: ['jshint:main', 'uglify'],
+        options: {
+          spawn: false,
+          livereload: true,
+        },
+      },
+      scss:{
+        files: ['src/stylesheets/*.scss', 'src/stylesheets/**/*.scss'],
+        tasks: ['sass', 'postcss'],
+        options: {
+          spawn: false,
+          livereload: true,
+        },
+      },
+      html: {
+        files: ['src/*.html'],
+        tasks: ['htmlhint'],
+        options: {
+          spawn: false,
+          livereload: true,
+        },
+      }/*,
+      php: {
+        files: ['src/*.php'],
+        tasks: [''], //to-do: add and configure php lint for grunt
+        options: {
+          spawn: false,
+          livereload: true,
+        },
+      }*/
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-modernizr');
+  grunt.loadNpmTasks('grunt-htmlhint');
+
+  // Default task.
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('compile', ['modernizr:dist', 'uglify', 'sass', 'postcss']);
+
+};
---
+++ wp-content/themes/csv-handbal/readme.md
@@ -0,0 +1,26 @@
+#Installation
+
+To install please make sure you have the following already installed globally on your system
+1. Node.js
+2. Bower
+2. Ruby and ruby gems
+3. Ruby sass gem
+
+To install open up this project root folder on the command line and launch the following commands:
+``
+npm install
+``
+the download all the bower dependencies
+`` 
+bower install
+``
+then let grunt compile all assets for the first time
+``
+grunt compile 
+``
+then run grunt watch to rerun tasks and live reload as we work on our project
+``
+grunt watch
+`` 
+
+## Have fun!
